6a9cab8006a612053aea125806762060e4e3fb08
RichFisher <rich.fishernc@gmail.com>
2018-12-03 20:46:14 -0600

    avatar uploading stuff

--- src/components/losers/losersImages.js
+++ src/components/losers/losersImages.js
@@ -7,11 +7,20 @@ const storage = firebase.storage();
 const mainRef = storage.ref();
 
 const selectAvatar = () => {
-
+  const avatarsArray = [];
+  mainRef.child().foreach((imageRef) => {
+    avatarsArray.push(imageRef.getDownloadURL());
+  });
+  let avatarString = '';
+  avatarsArray.forEach((avatarURL) => {
+    avatarString += `<image class='avatarImage' src='${avatarURL}' alt='Avatar failed to load' data-avatar-url='${avatarURL}'/>`;
+  });
+  $('#avatarSelectDiv').html(avatarString);
 };
 
 const addAvatar = () => {
-
+  let newAvatar = document.getElementById('addAvatarInput').files[0];
+  axios.post(`${apiKeys.firebaseKeys.storageBucket}`, newAvatar);
 };
 
 export default {
