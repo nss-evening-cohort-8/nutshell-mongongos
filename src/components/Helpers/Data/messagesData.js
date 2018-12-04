3821f27cc0acda33e02894e8822e66e4e20bfe53
MCrank <marco_mcse@hotmail.com>
2018-12-04 01:40:10 -0600

    WIP Message view, working on design

---
+++ src/components/Helpers/Data/messagesData.js
@@ -0,0 +1,7 @@
+// Author: Marco Crank
+// Purpose: Handle all the Axios call to Firebase for the Messages collection
+
+// import axios from 'axios';
+// import apiKeys from '../../../../db/apiKeys.json';
+
+// const fireBaseUrl = apiKeys.firebaseKeys.databaseURL;
---
+++ src/components/Messages/messages.js
@@ -0,0 +1,70 @@
+// Author: Marco Crank
+// Purpose: Handle all the Displaying and events related to the userName feature of the app.
+
+import $ from 'jquery';
+import 'bootstrap';
+import './messages.scss';
+// import usersData from '../Helpers/Data/usersData';
+
+const msgOutput = () => {
+  let newString = `
+  <div class="messages">
+  <div class="msg-history">
+  `;
+  // If NOT my UID then it is incoming
+  newString += `
+  <div class="incoming-msg">
+    <div class="incoming-msg-img"> <img alt="sunil" src="https://ptetutorials.com/images/user-profile.png"> </div>
+    <div class="received-msg">
+      <div class="received-width-msg">
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt inventore quod beatae blanditiis, eos molestias ipsam pariatur voluptates unde eaque adipisci maxime repellendus obcaecati nesciunt dolor nam quaerat, at praesentium.</p>
+        <span class="time-date"> 11:01 AM | June 9</span>
+      </div>
+    </div>
+  </div>
+  `;
+  // If it is my UID then it is outgoing
+  newString += `
+  <div class="outgoing-msg">
+    <div class="outgoing-msg-img"> <img alt="sunil" src="https://ptetutorials.com/images/user-profile.png"> </div>
+    <div class="sent-msg">
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt inventore quod beatae blanditiis, eos molestias ipsam pariatur voluptates unde eaque adipisci maxime repellendus obcaecati nesciunt dolor nam quaerat, at praesentium.</p>
+      <span class="time-date"> 11:01 AM | June 9 <i class="msg-del far fa-trash-alt fa-lg ml-2"></i><i class="msg-edit fas fa-edit fa-lg mr-2"></i></span>
+    </div>
+  </div>
+  <div class="outgoing-msg">
+    <div class="outgoing-msg-img"> <img alt="sunil" src="https://ptetutorials.com/images/user-profile.png"> </div>
+    <div class="sent-msg">
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt inventore quod beatae blanditiis, eos molestias ipsam pariatur voluptates unde eaque adipisci maxime repellendus obcaecati nesciunt dolor nam quaerat, at praesentium.</p>
+      <span class="time-date"> 11:01 AM | June 9 <i class="msg-del far fa-trash-alt fa-lg ml-2"></i><i class="msg-edit fas fa-edit fa-lg mr-2"></i></span>
+    </div>
+  </div>
+  <div class="outgoing-msg">
+    <div class="outgoing-msg-img"> <img alt="sunil" src="https://ptetutorials.com/images/user-profile.png"> </div>
+    <div class="sent-msg">
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt inventore quod beatae blanditiis, eos molestias ipsam pariatur voluptates unde eaque adipisci maxime repellendus obcaecati nesciunt dolor nam quaerat, at praesentium.</p>
+      <span class="time-date"> 11:01 AM | June 9 <i class="msg-del far fa-trash-alt fa-lg ml-2"></i><i class="msg-edit fas fa-edit fa-lg mr-2"></i></span>
+    </div>
+  </div>
+  <div class="outgoing-msg">
+    <div class="outgoing-msg-img"> <img alt="sunil" src="https://ptetutorials.com/images/user-profile.png"> </div>
+    <div class="sent-msg">
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt inventore quod beatae blanditiis, eos molestias ipsam pariatur voluptates unde eaque adipisci maxime repellendus obcaecati nesciunt dolor nam quaerat, at praesentium.</p>
+      <span class="time-date"> 11:01 AM | June 9 <i class="msg-del far fa-trash-alt fa-lg ml-2"></i><i class="msg-edit fas fa-edit fa-lg mr-2"></i></span>
+    </div>
+  </div>
+  `;
+  // newString += `
+  // <div class="type-msg">
+  //   <div class="input-msg-write">
+  //     <input class="write-msg" type="text" placeholder="Type a message">
+  // <button class="msg-send-btn" type="button">
+  // <i class="fas fa-upload" aria-hidden="true"></i></button>
+  //   </div>
+  // </div>
+  // `;
+  $('#message-container').html(newString);
+  // $('#message-input').html(newInputString)
+};
+
+export default { msgOutput };
