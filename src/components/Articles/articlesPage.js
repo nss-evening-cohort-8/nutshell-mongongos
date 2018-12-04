55776d67b07ffe83ce48d23f0182b7afe570939a
Michelle Beshears <meeshysu@gmail.com>
2018-12-03 19:55:41 -0600

    saving information so far to merge something else

---
+++ src/components/Articles/articlesPage.js
@@ -0,0 +1,18 @@
+import $ from 'jquery';
+import './articlesPage.scss';
+import articlesData from './articlesData';
+
+const printAllArticles = (allArticlesArray) => {
+  let domString = '';
+  allArticlesArray.forEach((article) => {
+    domString += `
+    <div class="news-articles-builder">
+      <h1 class="article-section">Articles</h1>
+      <h4 class="article-title">${article.title}</h4>
+      <hr class="my-4">
+      <p class="article-synopsis">${article.synopsis}</p>
+      <p class="article-url" href="${article.url}">Click here to view the article<p>
+    </div>
+    `;
+  });
+};
