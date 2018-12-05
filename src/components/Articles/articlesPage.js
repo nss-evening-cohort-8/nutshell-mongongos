bf4854e98ceef9832b8f1e163b192d9ce2ba5d7b
Michelle Beshears <meeshysu@gmail.com>
2018-12-04 21:39:26 -0600

    saving

--- src/components/Articles/articlesPage.js
+++ src/components/Articles/articlesPage.js
@@ -39,7 +39,25 @@ const articleComponent = () => {
 // FORM TO CREATE A NEW ARTICLE
 
 const formBuilder = (article) => {
-  const form = `
+  const form = ` <div id="modal-for-adding-articles"><div class="modal" tabindex="-1" role="dialog">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>Modal body text goes here.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
   <div class="article-form-group">
     <label for="form-article-name">Article Title:</label>
     <input type="text" class="title-input" value="${article.title}" id="title-input-form" placeholder="Coffee makers may contain mold!">
