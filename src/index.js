90e860add3b9eebf2c7e0282e53ddf4d1594f3a1
Jasmine Walters <jsmnwltrs@gmail.com>
2018-12-05 20:34:14 -0600

    zip codes can be deleted

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import users from './components/Users/users';
 import articlesPage from './components/Articles/articlesPage';
 import messages from './components/Messages/messages';
 import weather from './components/Weather/weather';
-import addDeleteWeather from './components/Weather/addDeleteWeather';
+import addWeather from './components/Weather/addWeather';
 
 
 import apiKeys from '../db/apiKeys.json';
@@ -22,7 +22,7 @@ const bindComponents = () => {
   articlesPage.articleComponent();
   weather.weatherComponent();
   weather.buildWeatherHeader();
-  addDeleteWeather.buildWeatherButtons();
+  addWeather.buildWeatherButtons();
 };
 
 const initialize = () => {
