200ce12685f4c775434d306a767e03204b812862
Mary West <westmarye1219@gmail.com>
2018-12-08 19:43:50 -0600

    modal styling

--- src/components/Events/AddEditEvents/addEditEvents.js
+++ src/components/Events/AddEditEvents/addEditEvents.js
@@ -1,25 +1,80 @@
 import $ from 'jquery';
 import 'bootstrap';
 import authHelpers from '../../Helpers/authHelpers';
-import initializeEventsPage from '../events';
+import events from '../events';
 import eventsData from '../EventsData/eventsData';
 
+// const addEventModalFormBuilder = (event) => {
+//   const domString = `<button type="button" class="btn btn-prima
+// ry" data-toggle="modal" data-target="#eventModal">Add A New Event</button>
+// <div class="modal fade" id="eventModal" tabindex="-1" role="di
+// alog" aria-labelledby="eventModal" aria-hidden="true">
+//   <div class="modal-dialog modal-dialog-centered" role="document">
+//     <div class="modal-content">
+//       <div class="modal-header">
+//         <h5 class="modal-title" id="eventModalHeader">Complete the form below:</h5>
+//         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+//           <span aria-hidden="true">&times;</span>
+//         </button>
+//       </div>
+//         <div class="modal-body">
+//         <label for="form-event-event">Event:</label>
+//         <input type="text" class="form-control " value ="${event.event}"
+// id="form-event-event" placeholder="Titans Game">
+//       </div>
+//       <div class="form-group">
+//         <label for="form-event-location">Location:</label>
+//         <input type="text" class="form-control" value ="${event.location}"
+// id="form-event-location" placeholder="NSS">
+//       </div>
+//       <div class="form-group">
+//       <label for="form-event-date">Date:</label>
+//       <div class="input-group date" id="datetimepicker1" data-target-input="nearest">
+//       <input type="text" class="form-control" value ="${event.startDate}"
+// id="form-event-date">
+//           <input type="text" class="form-control datetimepicker-input"
+// data-target="#datetimepicker1"/>
+//           <div class="input-group-append" data-target="#datetimepicker1"
+//  data-toggle="datetimepicker">
+//               <div class="input-group-text"><i class="fa fa-calendar"></i></div>
+//           </div>
+//         </div>
+//         <div class="modal-footer">
+//           <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+//           <button type="button" id="save-new-event" class="btn btn-primary">Save changes</button>
+//         </div>
+//     </div>
+//   </div>
+// </div>
+//     `;
+//   $('#add-edit-event').html(domString);
+// };
+
 const eventFormBuilder = (event) => {
-  const form = `
-    <div class="form-group">
+  const form = `<div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="editModalHeader">Complete the form below:</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+        <div class="modal-body"><div class="form-group">
       <label for="form-event-event">Event:</label>
-      <input type="text" class="form-control " value ="${event.event}"id="form-event-event" placeholder="Titans Game">
+      <input type="text" class="form-control " value ="${event.event}"id="form-event-event"
+      placeholder="Titans Game">
     </div>
     <div class="form-group">
       <label for="form-event-location">Location:</label>
-      <input type="text" class="form-control" value ="${event.location}" id="form-event-location" placeholder="NSS">
+      <input type="text" class="form-control" value ="${event.location}" id="form-event-location"
+      placeholder="NSS">
     </div>
     <div class="form-group">
     <label for="form-event-date">Date:</label>
     <div class="input-group date" id="datetimepicker1" data-target-input="nearest">
     <input type="text" class="form-control" value ="${event.startDate}" id="form-event-date">
-        <input type="text" class="form-control datetimepicker-input" data-target="#datetimepicker1"/>
-        <div class="input-group-append" data-target="#datetimepicker1" data-toggle="datetimepicker">
+        <input type="text" class="form-control datetimepicker-input"data-target="#datetimepicker1"/>
+        <div class="input-group-append" data-target="#datetimepicker1"data-toggle="datetimepicker">
             <div class="input-group-text"><i class="fa fa-calendar"></i></div>
         </div>
   </div>
@@ -27,15 +82,17 @@ const eventFormBuilder = (event) => {
   return form;
 };
 
-const gettingEventFromForm = () => {
-  const event = {
-    event: $('#form-event-event').val(),
-    location: $('#form-event-date').val(),
-    startDate: $('#form-event-location').val(),
-    userUid: authHelpers.getCurrentUid(),
-  };
-  return event;
-};
+/* <div class="modal" tabindex="-1" role="dialog">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body"> */
+
 
 const buildAddFrom = () => {
   const emptyEvent = {
@@ -49,6 +106,34 @@ const buildAddFrom = () => {
   $('#add-edit-event').html(domString).show();
   // $('#events-container').hide();
 };
+const buildEventButton = () => {
+  const domString = '<button id="add-event-button">+</button>';
+  $('#event-button').html(domString);
+  $('#add-event-button').on('click', buildAddFrom);
+};
+
+const gettingEventFromForm = () => {
+  const event = {
+    event: $('#form-event-event').val(),
+    location: $('#form-event-date').val(),
+    startDate: $('#form-event-location').val(),
+    userUid: authHelpers.getCurrentUid(),
+  };
+  return event;
+};
+
+// const buildAddFrom = () => {
+//   const emptyEvent = {
+//     event: '',
+//     startDate: '',
+//     location: '',
+//   };
+//   let domString = '<h2>Add New Event</h2>';
+//   domString += eventFormBuilder(emptyEvent);
+//   domString += '<button id="add-event">Add Event</button>';
+//   $('#add-edit-event').html(domString).show();
+//   // $('#events-container').hide();
+// };
 
 const addNewEvent = () => {
   const newEvent = gettingEventFromForm();
@@ -56,19 +141,20 @@ const addNewEvent = () => {
     .then(() => {
       $('#add-edit-event').html('').hide();
       $('#events-container').show();
-      initializeEventsPage();
+      events.initializeEventsPage();
       eventFormBuilder();
+      events.printAllEvents();
     })
     .catch((error) => {
       console.error('error', error);
     });
 };
 
-const buildEventButton = () => {
-  const domString = '<button id="add-event-button">+</button>';
-  $('#event-button').html(domString);
-  $('#add-event-button').on('click', buildAddFrom);
-};
+// const buildEventButton = () => {
+//   const domString = '<button id="add-event-button">+</button>';
+//   $('#event-button').html(domString);
+//   $('#add-event-button').on('click', buildAddFrom);
+// };
 
 const showEditEventForm = (e) => {
   const idToEdit = e.target.dataset.editId;
@@ -77,9 +163,10 @@ const showEditEventForm = (e) => {
       let domString = '<h2>Edit Event</h2>';
       domString += eventFormBuilder(singleEvent);
       domString += `<button id="edit-event" data-single-edit-id=${singleEvent.id}>Save Event</button>`;
-      $('#add-edit-event').html(domString).show();
+      // $('#add-edit-event').html(domString).show();
+      $('#eventModal').html(domString).show();
       // $('#events-container').hide();
-      $('#events-modal-container').hide();
+      $('#events-modal-container').show();
     })
     .catch((error) => {
       console.error('error in getting single for edit', error);
@@ -94,7 +181,7 @@ const updateEvent = (e) => {
       $('#add-edit-event').html('').hide();
       // $('#single-container').html('');
       // $('#events-container').show();
-      initializeEventsPage();
+      events.initializeEventsPage();
     })
     .catch((error) => {
       console.error('error', error);
--- src/components/Events/events.js
+++ src/components/Events/events.js
@@ -4,7 +4,6 @@ import eventsData from './EventsData/eventsData';
 import authHelpers from '../Helpers/authHelpers';
 
 const printSingleEvent = (event) => {
-  console.log('event', event);
   const eventString = `
     <div>
         <h1>${event.event}</h1>
@@ -18,6 +17,26 @@ const printSingleEvent = (event) => {
   $('#single-container').hide();
 };
 
+// const printAllEvents = (allEventsArray) => {
+//   let domString = `<h2>Events</h2>
+// `;
+//   allEventsArray.forEach((event) => {
+//     domString += `
+//     <div class="evnts-builder">
+//       <button type="button" id="delete-event-button" class="btn btn btn-sm"
+// data-delete-id=${event.id}>X</button>
+//       <button type="button" id="edit-event-button" class="btn btn btn-sm
+// edit-btn" data-edit-id=${event.id}>Edit</button>
+//       <h5 class="event-event">&#9758 ${event.event}</h5>
+//       <p class="event-location">${event.location}</p>
+//       <a class="event-startDate" href="${event.startDate}" target="_blank"></a>
+//     </button>
+//     <div id="${event.id}"></div>
+//     `;
+//     $('#events-modal-container').html(domString);
+//   });
+// };
+
 const getSingleEvent = (e) => {
   const eventId = e.target.dataset.dropdownId;
   const uid = authHelpers.getCurrentUid();
@@ -30,58 +49,39 @@ const getSingleEvent = (e) => {
       console.error('error in getting one event', error);
     });
 };
+
 const buildModal = (eventsArray) => {
   let modal = `<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#eventModal">
-  Pick Event
-</button>
-<!-- Modal -->
-<div class="modal fade" id="eventModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="eventModalLabel">Events</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">`;
+    Pick Event
+  </button>
+  <!-- Modal -->
+  <div class="modal fade" id="eventModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="eventModalLabel">Events</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">`;
   if (eventsArray.length) {
     eventsArray.forEach((event) => {
-      modal += `<div class="modal-item get-single" data-modal-id=${event.id}>
-      <div class = "container">
-      <div class = "row justify-content-center">
-      <div class="col-sm-4">
-      ${event.event}
-      </div>
-      </div>
-      <div class = "row justify-content-center">
-      <div class="col-sm-4">
-      ${event.location}
-      </div>
-      </div>
-      <div class = "row justify-content-center">
-      <div class="col-sm-4">
-      ${event.startDate}</div>
-      </div>
-      </div>
-      <div class="modal-footer justify-content-center">
-        <button type="button" class="btn delete-btn-event btn-danger" data-delete-id=${event.id}>X</button>
-        <button type="button" class="btn edit-btn-event btn-primary" data-edit-id=${event.id}>/</button>
-        </div>
-      </div>`;
+      modal += `<div class="modal-item get-single" data-modal-id=${event.id}>${event.event}${event.location}${event.startDate}</div>
+        <div class="modal-footer">
+          <button type="button" class="btn delete-btn-event btn-danger" data-delete-id=${event.id}>X</button>
+          <button type="button" class="btn edit-btn-event btn-primary" data-edit-id=${event.id}>/</button>
+        </div>`;
     });
   } else {
     modal += '<div class = "modal-item">You have no events.</div>';
   }
   modal += `</div>
-      </div>
-      </div>;`;
-
+        </div>
+        </div>;`;
   modal += '</div></div>';
   $('#events-modal-container').html(modal);
-  getSingleEvent();
 };
-
 const eventsComponent = () => {
   const uid = authHelpers.getCurrentUid();
   eventsData.getAllEvents(uid)
@@ -114,4 +114,5 @@ const initializeEventsPage = () => {
   eventsComponent();
   bindEvents();
 };
+
 export default initializeEventsPage;
